ec25e64b7060286f6b983bf3833319e11da1d1b7
Deepak Malpani <deepakmalpani0@gmail.com>
2026-05-12 11:51:48 +0530

    Add StaticDatabase in-memory collections
    Introduce StaticDatabase class (Data/StaticDatabase.cs) that provides in-memory collections for workout data: workoutPlans, workoutDays, exercises, and workoutLogs. Adds List<T> properties initialized to empty collections to serve as a simple static datastore for the application.

---
+++ Data/StaticDatabase.cs
@@ -0,0 +1,7 @@
+public class StaticDatabase
+{
+    public List<WorkoutPlan> workoutPlans {get; set;} = [];
+    public List<WorkoutDay> workoutDays {get; set;} = [];
+    public List<Exercise> exercises {get; set;} = [];
+    public List<WorkoutLog> workoutLogs {get; set;} = [];
+}
