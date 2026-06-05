13f9cb44c865ef0d35437e79adb9e87df49fba2f
Deepak Malpani <deepakmalpani0@gmail.com>
2026-06-05 11:54:30 +0530

    Add WorkoutDaysEndpoints to handle retrieval and creation of workout days

---
+++ Endpoints/WorkoutDaysEndpoints.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+public static class WorkoutDaysEndpoints
+{
+    public static void RegisterWorkoutDaysEndpoints(this WebApplication app)
+    {
+        app.MapGet("/{planId}/days", async (int planId, [FromBody] List<CreateWorkoutDay> workoutDays) =>
+        {
+            if (workoutDays == null || !workoutDays.Any())
+        {
+            return Results.BadRequest("Workout days list cannot be null or empty");
+        }
+        var parentWorkoutPlan = StaticDatabase.WorkoutPlans.FirstOrDefault(m => m.Id == planId);
+        if (parentWorkoutPlan == null)
+        {
+            return NotFound($"Plan with ID {planId} does not exist");
+        }
+        int nextId = StaticDatabase.WorkoutDays.Any() ?
+                        StaticDatabase.WorkoutDays.Max(m => m.Id) + 1 :
+                        1;
+        var newWorkoutDayList = new List<WorkoutDay>();
+        foreach (var workoutDay in workoutDays)
+        {
+            var newWorkoutDay = new WorkoutDay()
+            {
+                Id = nextId++,
+                WorkoutPlanId = parentWorkoutPlan.Id,
+                DayOfWeek = workoutDay.DayOfWeek,
+                Name = workoutDay.Name,
+                IsRestDay = workoutDay.IsRestDay
+            };
+            newWorkoutDayList.Add(newWorkoutDay);
+        }
+        StaticDatabase.WorkoutDays.AddRange(newWorkoutDayList);
+        return CreatedAtAction(nameof(CreateWorkoutDay), new { planId = planId }, newWorkoutDayList);
+        });
+    }
+}
